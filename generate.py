4566500e1b64ff3bf8af62aabf01c75feecf1bd5
NohanJoemon <nohanjoemon253@gmail.com>
2022-06-20 19:00:45 +0530

    Added

--- generate.py
+++ generate.py
@@ -35,9 +35,6 @@ def gen_sequence(gen_seq_length,wpath,path_char_to_index):
 
 
 def convert_to_midi(abc,writepath):
-    if os.path.exists(writepath):
-        os.remove(writepath)
-
     c = converter.subConverters.ConverterABC()
     c.registerOutputExtensions = ("midi", )
     c.parseData(abc)
