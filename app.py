b25c6a11d590514e1668a5de913527d6897cf3b1
NohanJoemon <nohanjoemon253@gmail.com>
2022-06-20 18:37:13 +0530

    Added

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 wpath = os.path.join(APP_ROOT,"static/weights.80.hdf5")
 path_char_to_index = os.path.join(APP_ROOT,"static/char_to_index.json")
-mpath = os.path.join(APP_ROOT,"static\music.mid")
+mpath = os.path.join(APP_ROOT,"static/music.mid")
 
 @app.route("/", methods=["GET","POST"])
 def predict():
@@ -20,8 +20,8 @@ def predict():
         writepath = predictor.predict(seq_length,wpath,path_char_to_index,mpath)
         pred=1
         selected=[seq_length]
-
-    return render_template("index.html",pred=pred, selected=selected)
+    print(writepath)
+    return render_template("index.html",pred=pred, selected=selected,writepath=mpath)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=5000,debug=True)
