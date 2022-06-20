48f8b808956f0f627d840e25d927305d5169c8dc
NohanJoemon <nohanjoemon253@gmail.com>
2022-06-20 18:44:27 +0530

    Added

--- predictor.py
+++ predictor.py
@@ -1,10 +1,7 @@
 from generate import gen_sequence,convert_to_midi
 
 def predict(gen_seq_length,wpath,path_char_to_index,mpath):
-    # seq = gen_sequence(gen_seq_length,wpath,path_char_to_index)
-    # print(seq)
-    # convert_to_midi(seq,mpath)
-    # return mpath
+
     while True:
         try:
             seq = gen_sequence(gen_seq_length,wpath,path_char_to_index)
